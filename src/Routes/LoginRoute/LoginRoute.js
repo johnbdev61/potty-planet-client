7d19108f00c654600f2ea8f6b42ff8423c38db0e
John Byrne <jbyrne87@gmail.com>
2021-01-31 21:21:01 -0600

    first commit

---
+++ src/Routes/LoginRoute/LoginRoute.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LoginRoute extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+      </div>
+    )
+  }
+}
---
+++ src/Routes/RegisterRoute/RegisterRoute.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RegisterRoute extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Register</h2>
+      </div>
+    )
+  }
+}
