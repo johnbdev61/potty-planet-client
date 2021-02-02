7c9a1ac175ec7e0251da7b50d904158e0fa422fd
John Byrne <jbyrne87@gmail.com>
2021-02-01 21:27:56 -0600

    set up post service functions

--- src/Routes/HomeRoute/HomeRoute.js
+++ src/Routes/HomeRoute/HomeRoute.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Token from '../../Services/token-service'
+import config from '../../config'
 
 export default class HomeRoute extends Component {
   render() {
---
+++ src/Services/post-api-service.js
@@ -0,0 +1,47 @@
+import TokenService from '../Services/token-service'
+import config from '../config'
+
+const PostApiService = {
+  getPosts() {
+    return fetch(`${config.API_ENDPOINT}/posts`, {
+      headers: {},
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    )
+  },
+  getPost(postId) {
+    return fetch(`${config.API_ENDPOINT}/posts/${postId}`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    )
+  },
+  getPostComments(postId) {
+    return fetch(`${config.API_ENDPOINT}/posts/${postId}/comments`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    )
+  },
+  postComment(postId, text) {
+    return fetch(`${config.API_ENDPOINT}/comments`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        post_id: postId,
+        comment: comment,
+      }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    )
+  },
+}
+
+export default PostApiService
